8ad1649fea72e22283e5f77578a14e9ea7e451e3
AndersonHJB <cleland1432803776@icloud.com>
2022-11-09 03:15:24 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,11 @@ git status
 git add .
 git commit . -m "update"
 git push -u origin main
+yarn run docs:build
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f root@8.142.40.205:/home/www/website/ts.git main
+cd -
