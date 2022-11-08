76f1cd62e2e927fac79cf9caaa6188d34dd22726
AndersonHJB <cleland1432803776@icloud.com>
2022-11-09 00:01:11 +0800

    update

--- docs/.vuepress/.temp/components/config.js
+++ docs/.vuepress/.temp/components/config.js
@@ -1,13 +1,13 @@
 import { defineClientConfig } from "@vuepress/client";
 import { h } from "vue";
 
-import { useStyleTag } from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/vueuse.js";
-import FontIcon from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/components/FontIcon.js";
-import PDF from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/components/PDF.js";
-import YouTube from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/components/YouTube.js";
-import VideoPlayer from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/components/VideoPlayer.js";
-import BiliBili from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/components/BiliBili.js";
-import BackToTop from "/Users/huangjiabao/GitHub/WebSite/BornforthisHJB.github.io/node_modules/vuepress-plugin-components/lib/client/components/BackToTop.js";
+import { useStyleTag } from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/vueuse.js";
+import FontIcon from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/components/FontIcon.js";
+import PDF from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/components/PDF.js";
+import YouTube from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/components/YouTube.js";
+import VideoPlayer from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/components/VideoPlayer.js";
+import BiliBili from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/components/BiliBili.js";
+import BackToTop from "/Users/huangjiabao/GitHub/WebSite/ShuaiShao20010902.github.io/node_modules/vuepress-plugin-components/lib/client/components/BackToTop.js";
 
 
 export default defineClientConfig({
--- docs/.vuepress/.temp/pages/category/python-auto/index.html.js
+++ docs/.vuepress/.temp/pages/category/python-auto/index.html.js
@@ -1,1 +1,14 @@
-export const data = JSON.parse("{\"key\":\"v-826a4772\",\"path\":\"/category/python-auto/\",\"title\":\"Python auto 分类\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Python auto 分类\",\"blog\":{\"type\":\"category\",\"name\":\"Python auto\",\"key\":\"category\"},\"layout\":\"Blog\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://bornforthis.cn/category/python-auto/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"AI悦创\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Python auto 分类\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"git\":{},\"readingTime\":{\"minutes\":0,\"words\":0},\"copyright\":\"著作权归黄家宝|AI悦创所有\\n原文链接：https://bornforthis.cn/category/python-auto/\",\"filePathRelative\":null}")
+export const data = JSON.parse("{\"key\":\"v-826a4772\",\"path\":\"/category/python-auto/\",\"title\":\"Python auto 分类\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Python auto 分类\",\"blog\":{\"type\":\"category\",\"name\":\"Python auto\",\"key\":\"category\"},\"layout\":\"Blog\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://bornforthis.cn/category/python-auto/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"AI悦创\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Python auto 分类\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":0},\"copyright\":\"著作权归黄家宝|AI悦创所有\\n原文链接：https://bornforthis.cn/category/python-auto/\",\"filePathRelative\":null}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
